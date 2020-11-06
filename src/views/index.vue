a73d5e1759ed7505bb5e8087412bf37ad2b0a354
Lin <123489@126.com>
2020-11-06 12:06:38 +0800

    完善一点的锤子商城

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,15 +3,23 @@
     <div class="sku-box store-content">
       <div class="sort-option">
         <ul class="line clear">
-          <li><a href="javascript:;" class="active">综合排序</a></li>
-          <li><a href="javascript:;" class="">销量排序</a></li>
-          <li><a href="javascript:;" class="">价格低到高</a></li>
-          <li><a href="javascript:;" class="">价格高到低</a></li>
+          <li v-for="(item, i) in title" :key="i">
+            <a
+              href="javascript:;"
+              :class="{ active: isActive === i }"
+              @click="sortShop(i, item)"
+              >{{ item }}</a
+            >
+          </li>
         </ul>
       </div>
       <div class="gray-box">
         <div class="item-box">
-          <item-shop v-for="item in itemDataList" :key="item.id" :list="item"></item-shop>
+          <item-shop
+            v-for="item in itemSortDataList"
+            :key="item.id"
+            :list="item"
+          ></item-shop>
         </div>
       </div>
     </div>
@@ -19,15 +27,24 @@
 </template>
 
 <script>
-import itemShop from '@/components/ItemShop';
+import itemShop from "@/components/ItemShop";
 export default {
   data() {
     return {
-      itemDataList: []
+      //请求回的所有商品数据
+      itemDataList: [],
+      //排序标题
+      title: ["综合排序", "销量排序", "价格低到高", "价格高到低"],
+      //控制类名
+      isActive: 0,
+      //排序前的数据(排序后会让请求回的原数据也发生改变,需要一个值储存原来的数据)
+      itemOldDataList: [],
+      //排序后商品数据
+      itemSortDataList: [],
     };
   },
-  components:{
-    itemShop
+  components: {
+    itemShop,
   },
   async created() {
     //或者使用浏览器的API
@@ -35,11 +52,45 @@ export default {
     // let data =await res.json();
     // console.log(data);
 
+    
     let { data } = await this.$axios.get("/static/goodData.json");
-    this.itemDataList = data.map(item=>{
-      return item.spu
+     this.itemSortDataList = data.map((item) => {
+      return item.spu;
     });
-    //console.log(this.itemDataList);
+    this.itemOldDataList = data.map((item) => {
+      return item.spu;
+    });
+    this.itemDataList = data.map((item) => {
+      return item.spu;
+    });
+
+  },
+  methods: {
+    sortShop(i, item) {
+      this.isActive = i;
+      switch (item) {
+        case "综合排序":
+          this.itemSortDataList = this.itemOldDataList;
+          break;
+        case "价格低到高":
+          this.itemSortDataList = this.itemDataList.sort(function (a, b) {
+            a = a.sku_info[0].price;
+            b = b.sku_info[0].price;
+            return a - b;
+          });
+          break;
+        case "价格高到低":
+          this.itemSortDataList = this.itemDataList.sort(function (a, b) {
+            a = a.sku_info[0].price;
+            b = b.sku_info[0].price;
+            return b - a;
+          });
+          break;
+        default:
+          this.itemSortDataList = this.itemOldDataList;
+          break;
+      }
+    },
   },
 };
 </script>
